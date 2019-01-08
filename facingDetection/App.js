f1bb6a424c24389a5a54e482d16cb41036806b34
Timn97 <d2fwind@gmail.com>
2019-01-09 01:07:06 +0700

    Prepare for basic tag in react-native - ThangDP

--- facingDetection/App.js
+++ facingDetection/App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,13 +17,56 @@ const instructions = Platform.select({
 });
 
 
+
+
+
+
+
 export default class App extends Component {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val, // set placeName equal val
+    });
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === ""){
+      return;
+    }
+
+    this.setState(preStave => {
+      return{
+        places: preStave.places.concat(preStave.placeName)
+      };
+    })
+  };
+
+
   render() {
+    const placesOutput = this.state.places.map(
+      (place,i) => (<Text key={i}>{place}</Text>)
+    )
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          placeholder="Input your name !"
+          value={this.state.placeName}
+          onChangeText={this.placeNameChangeHandler}
+          style={styles.inputTag}
+        />
+        <View style = {styles.buttonContainer}>
+        <Button 
+          title = 'Add'
+          style = {{marginRight: 30}}
+          onPress = {this.placeSubmitHandler}
+        />
+        </View>
+        <View>{placesOutput}</View>
       </View>
     );
   }
@@ -31,8 +74,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    //flex: 1,
+    //justifyContent: 'space-between',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -46,4 +89,17 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+
+  inputTag: {
+    width: 300, 
+    borderColor: 'lightgray', 
+    borderWidth: 1
+  },
+  buttonContainer: {
+    //flex:1,
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    
+
+  }
 });
