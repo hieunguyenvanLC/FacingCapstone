7690c99835264b8cae702733992f8512ee945db1
atgmep <whalebun@gmail.com>
2019-02-03 21:32:49 +0700

    Create fps project - NghiaTT

---
+++ fps/src/main/java/capstone/fps/FpsApplication.java
@@ -0,0 +1,14 @@
+package capstone.fps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpsApplication.class, args);
+    }
+
+}
+
