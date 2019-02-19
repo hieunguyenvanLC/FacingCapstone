83dbc9da8afa1022d697f91970a9f113a8a42f22
LamNH <lamnhse61234@fpt.edu.vn>
2019-02-19 19:32:41 +0700

    Delete trash file-<Lamnh>
    Make folder cleaner

--- Nguyen Huu Lam/lan3/src/main/java/tuhoc/lan3/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tuhoc.lan3.repository;
-
-import tuhoc.lan3.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
