1458189befb9029ea1a78662bb75164a4772dd20
atgmep <whalebun@gmail.com>
2019-03-13 16:21:44 +0700

    .

--- fps/src/main/java/capstone/fps/config/WebConfig.java
+++ fps/src/main/java/capstone/fps/config/WebConfig.java
@@ -1,17 +1,11 @@
 package capstone.fps.config;
 
 import capstone.fps.model.AppData;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig {
 
