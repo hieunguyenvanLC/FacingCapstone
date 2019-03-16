1b088e50e1daf5972608a4bf5e01dd71f5d13a3d
Corner Nguyen <vanhieu.nguyen152@gmail.com>
2019-03-16 19:07:40 +0700

    config allow origin localhost for Mobile- HieuNV

--- fps/src/main/java/capstone/fps/config/WebSecurityConfig.java
+++ fps/src/main/java/capstone/fps/config/WebSecurityConfig.java
@@ -116,7 +116,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8100"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8100", "http://localhost"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS","PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type","Access-Control-Allow-Credentials"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
