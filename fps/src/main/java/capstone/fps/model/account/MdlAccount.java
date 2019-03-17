c66e0da2e8c9de3f1346993c7a53fb40b920994b
atgmep <whalebun@gmail.com>
2019-03-17 20:49:14 +0700

    Update MdlAccount.java-NghiaTT

--- fps/src/main/java/capstone/fps/model/account/MdlAccount.java
+++ fps/src/main/java/capstone/fps/model/account/MdlAccount.java
@@ -43,181 +43,181 @@ public class MdlAccount {
     @Expose
     String avatar;
 
-    public MdlAccount() {
-
-    }
-
-    public MdlAccount(Integer id, String phone, String role, String password, String name, String email, Integer extraPoint, Integer reportPoint, String userImage, String natId, Long natDate, Long dob, Long createTime, Long updateTime, Long deleteTime, String note, Integer status, String editor, String avatar) {
-        this.id = id;
-        this.phone = phone;
-        this.role = role;
-        this.password = password;
-        this.name = name;
-        this.email = email;
-        this.extraPoint = extraPoint;
-        this.reportPoint = reportPoint;
-        this.userImage = userImage;
-        this.natId = natId;
-        this.natDate = natDate;
-        this.dob = dob;
-        this.createTime = createTime;
-        this.updateTime = updateTime;
-        this.deleteTime = deleteTime;
-        this.note = note;
-        this.status = status;
-        this.editor = editor;
-        this.avatar = avatar;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getExtraPoint() {
-        return extraPoint;
-    }
-
-    public void setExtraPoint(Integer extraPoint) {
-        this.extraPoint = extraPoint;
-    }
-
-    public Integer getReportPoint() {
-        return reportPoint;
-    }
-
-    public void setReportPoint(Integer reportPoint) {
-        this.reportPoint = reportPoint;
-    }
-
-    public String getUserImage() {
-        return userImage;
-    }
-
-    public void setUserImage(String userImage) {
-        this.userImage = userImage;
-    }
-
-    public String getNatId() {
-        return natId;
-    }
-
-    public void setNatId(String natId) {
-        this.natId = natId;
-    }
-
-    public Long getNatDate() {
-        return natDate;
-    }
-
-    public void setNatDate(Long natDate) {
-        this.natDate = natDate;
-    }
-
-    public Long getDob() {
-        return dob;
-    }
-
-    public void setDob(Long dob) {
-        this.dob = dob;
-    }
-
-    public Long getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Long createTime) {
-        this.createTime = createTime;
-    }
-
-    public Long getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Long updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Long getDeleteTime() {
-        return deleteTime;
-    }
-
-    public void setDeleteTime(Long deleteTime) {
-        this.deleteTime = deleteTime;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getEditor() {
-        return editor;
-    }
-
-    public void setEditor(String editor) {
-        this.editor = editor;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
+//    public MdlAccount() {
+//
+//    }
+//
+//    public MdlAccount(Integer id, String phone, String role, String password, String name, String email, Integer extraPoint, Integer reportPoint, String userImage, String natId, Long natDate, Long dob, Long createTime, Long updateTime, Long deleteTime, String note, Integer status, String editor, String avatar) {
+//        this.id = id;
+//        this.phone = phone;
+//        this.role = role;
+//        this.password = password;
+//        this.name = name;
+//        this.email = email;
+//        this.extraPoint = extraPoint;
+//        this.reportPoint = reportPoint;
+//        this.userImage = userImage;
+//        this.natId = natId;
+//        this.natDate = natDate;
+//        this.dob = dob;
+//        this.createTime = createTime;
+//        this.updateTime = updateTime;
+//        this.deleteTime = deleteTime;
+//        this.note = note;
+//        this.status = status;
+//        this.editor = editor;
+//        this.avatar = avatar;
+//    }
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public Integer getExtraPoint() {
+//        return extraPoint;
+//    }
+//
+//    public void setExtraPoint(Integer extraPoint) {
+//        this.extraPoint = extraPoint;
+//    }
+//
+//    public Integer getReportPoint() {
+//        return reportPoint;
+//    }
+//
+//    public void setReportPoint(Integer reportPoint) {
+//        this.reportPoint = reportPoint;
+//    }
+//
+//    public String getUserImage() {
+//        return userImage;
+//    }
+//
+//    public void setUserImage(String userImage) {
+//        this.userImage = userImage;
+//    }
+//
+//    public String getNatId() {
+//        return natId;
+//    }
+//
+//    public void setNatId(String natId) {
+//        this.natId = natId;
+//    }
+//
+//    public Long getNatDate() {
+//        return natDate;
+//    }
+//
+//    public void setNatDate(Long natDate) {
+//        this.natDate = natDate;
+//    }
+//
+//    public Long getDob() {
+//        return dob;
+//    }
+//
+//    public void setDob(Long dob) {
+//        this.dob = dob;
+//    }
+//
+//    public Long getCreateTime() {
+//        return createTime;
+//    }
+//
+//    public void setCreateTime(Long createTime) {
+//        this.createTime = createTime;
+//    }
+//
+//    public Long getUpdateTime() {
+//        return updateTime;
+//    }
+//
+//    public void setUpdateTime(Long updateTime) {
+//        this.updateTime = updateTime;
+//    }
+//
+//    public Long getDeleteTime() {
+//        return deleteTime;
+//    }
+//
+//    public void setDeleteTime(Long deleteTime) {
+//        this.deleteTime = deleteTime;
+//    }
+//
+//    public String getNote() {
+//        return note;
+//    }
+//
+//    public void setNote(String note) {
+//        this.note = note;
+//    }
+//
+//    public Integer getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(Integer status) {
+//        this.status = status;
+//    }
+//
+//    public String getEditor() {
+//        return editor;
+//    }
+//
+//    public void setEditor(String editor) {
+//        this.editor = editor;
+//    }
+//
+//    public String getAvatar() {
+//        return avatar;
+//    }
+//
+//    public void setAvatar(String avatar) {
+//        this.avatar = avatar;
+//    }
 }
