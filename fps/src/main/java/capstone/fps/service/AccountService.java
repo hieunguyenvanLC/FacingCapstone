fb52806f8cb587adbffaf9bb03d96ad7d12a0dc3
Corner Nguyen <vanhieu.nguyen152@gmail.com>
2019-03-23 17:34:12 +0700

    update write file image - HieuNV

--- fps/src/main/java/capstone/fps/service/AccountService.java
+++ fps/src/main/java/capstone/fps/service/AccountService.java
@@ -1,9 +1,6 @@
 package capstone.fps.service;
 
-import capstone.fps.common.Fix;
-import capstone.fps.common.Methods;
-import capstone.fps.common.Repo;
-import capstone.fps.common.Validator;
+import capstone.fps.common.*;
 import capstone.fps.entity.*;
 import capstone.fps.model.Response;
 import capstone.fps.model.account.*;
@@ -12,7 +9,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -443,7 +445,7 @@ public class AccountService {
     // Web - Shipper - End
 
     // Mobile Member - Register - Begin
-    public Response createAccountMember(String phone, String pass, String name, MultipartFile face) {
+    public Response createAccountMember(String phone, String pass, String name, MultipartFile face) throws IOException {
         Methods methods = new Methods();
         Validator valid = new Validator();
         Response response = new Response<>(Response.STATUS_FAIL, Response.MESSAGE_FAIL);
@@ -472,12 +474,25 @@ public class AccountService {
         frAccount.setEditor(null);
         accountRepo.save(frAccount);
         // train here
-
+//        trainningFaceRecognise(face);
 
         response.setResponse(Response.STATUS_SUCCESS, Response.MESSAGE_SUCCESS);
         return response;
     }
-    // Mobile Member - Register - End
+
+    public  void trainningFaceRecognise(MultipartFile face) throws IOException {
+        CommandPrompt commandPrompt = new CommandPrompt();
+
+        InputStream in = new ByteArrayInputStream(face.getBytes());
+
+        BufferedImage image = ImageIO.read(in);
+//        Path file = Paths.get();
+        File outputFile = new File("/Users/nguyenvanhieu/Project/CapstoneProject/docker/data/test/Vladimir_Putin/hieu.jpeg");
+        ImageIO.write(image, "png", outputFile);
+    }
+//        String
+
+        // Mobile Member - Register - End
 
     // Mobile Mem - Profile - Begin
     public Response<MdlAccount> getMemberDetailMem() {
